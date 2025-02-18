c8b7382c46ff6cb35f54065e364c4a056004089a
Max Bronner <88713269+max-bronner@users.noreply.github.com>
2025-02-18 21:48:27 +0100

    Add sample content

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,38 @@ import Handlebars from 'vite-plugin-handlebars';
 
 const pageData = {
   '/index.html': {
+    articles: [
+      {
+        title: 'An interesting title for a blog post or something',
+        description:
+          'A short description of the blog post which is rather long and should give a good idea of what the post is about. It can even have several sentences.',
+        date: '2025-01-01',
+      },
+      {
+        title: 'Another title for a blog post',
+        description:
+          'A short description of the blog post which is rather long and should give a good idea of what the post is about. It can even have several sentences.',
+        date: '2025-01-01',
+      },
+      {
+        title: 'Ohh, is this also a title for a blog post?',
+        description:
+          'A short description of the blog post which is rather long and should give a good idea of what the post is about. It can even have several sentences.',
+        date: '2025-01-01',
+      },
+      {
+        title: 'Ok, now it becomes boring with these titles',
+        description:
+          'A short description of the blog post which is rather long and should give a good idea of what the post is about. It can even have several sentences.',
+        date: '2025-01-01',
+      },
+      {
+        title: 'Seriously, another title for a blog post?',
+        description:
+          'A short description of the blog post which is rather long and should give a good idea of what the post is about. It can even have several sentences.',
+        date: '2025-01-01',
+      },
+    ],
     title: 'Main Page',
   },
   '/about/index.html': {
