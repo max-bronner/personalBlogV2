09153527f40c0ccd0115605e7b79c4c05cfe7226
Max Bronner <88713269+max-bronner@users.noreply.github.com>
2025-02-18 13:41:56 +0100

    Remove pug

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,6 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import { ViteMinifyPlugin } from 'vite-plugin-minify';
-import pugPlugin from 'vite-plugin-pug';
-
-const options = { localImports: true };
-const locals = { name: 'My Pug' };
 
 export default defineConfig({
   root: 'src',
@@ -27,6 +23,5 @@ export default defineConfig({
       removeRedundantAttributes: true,
       removeEmptyAttributes: true,
     }),
-    pugPlugin(options, locals),
   ],
 });
