2dcc28e383de424d710e4898ee4d27c1072a70f1
Max Bronner <88713269+max-bronner@users.noreply.github.com>
2025-02-26 20:29:20 +0100

    Add additional content for recent posts

--- src/blog/posts/index.js
+++ src/blog/posts/index.js
@@ -1,7 +1,15 @@
 import data20200606 from './plants_2020-06-06.json';
+import data20200607 from './plants_2020-06-07.json';
+import data20200608 from './plants_2020-06-08.json';
+import data20200609 from './plants_2020-06-09.json';
+import data20200610 from './plants_2020-06-10.json';
 
 const data = [
   data20200606,
+  data20200607,
+  data20200608,
+  data20200609,
+  data20200610,
 ];
 
 export default data.sort((a, b) => {
