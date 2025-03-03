fa7b324cca9c08e1a4966f103775699971809bfb
Max Bronner <88713269+max-bronner@users.noreply.github.com>
2025-03-03 22:57:17 +0100

    Migrate hiking post

--- src/blog/posts/data.js
+++ src/blog/posts/data.js
@@ -1,8 +1,7 @@
 import data20200606 from './plants_2020-06-06.json';
+import data20200615 from './plants_2020-06-15.json';
 
-const data = [
-  data20200607,
-];
+const data = [data20200606, data20200615, data20201109];
 
 export default data.sort((a, b) => {
   return new Date(b.date) - new Date(a.date);
--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import Handlebars from 'vite-plugin-handlebars';
 import { HotReloadHbs, helpers } from './vite.plugin.js';
 import dataHome from './src/data.js';
 import data20200606 from './src/blog/posts/plants_2020-06-06.json';
+import data20200615 from './src/blog/posts/plants_2020-06-15.json';
 import dataBlog from './src/blog/posts/data.js';
 import fs from 'fs';
 
@@ -16,6 +17,7 @@ const pageData = {
   '/index.html': dataHome,
   '/blog/index.html': { articles: dataBlog },
   '/blog/plants_2020-06-06.html': data20200606,
+  '/blog/plants_2020-06-15.html': data20200615,
   '/about/index.html': {
     title: 'About',
   },
@@ -31,6 +33,7 @@ export default defineConfig({
         main: resolve(__dirname, 'src/index.html'),
         blog: resolve(__dirname, 'src/blog/index.html'),
         post1: resolve(__dirname, 'src/blog/plants_2020-06-06.html'),
+        post2: resolve(__dirname, 'src/blog/plants_2020-06-15.html'),
         about: resolve(__dirname, 'src/about/index.html'),
       },
     },
