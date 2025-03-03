bdde946faa98d8ff09e4919b7646efb493d37f7a
Max Bronner <88713269+max-bronner@users.noreply.github.com>
2025-03-03 22:54:58 +0100

    Remove duplicated posts

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,6 @@ import Handlebars from 'vite-plugin-handlebars';
 import { HotReloadHbs, helpers } from './vite.plugin.js';
 import dataHome from './src/data.js';
 import data20200606 from './src/blog/posts/plants_2020-06-06.json';
-import data20200607 from './src/blog/posts/plants_2020-06-07.json';
-import data20200608 from './src/blog/posts/plants_2020-06-08.json';
-import data20200609 from './src/blog/posts/plants_2020-06-09.json';
-import data20200610 from './src/blog/posts/plants_2020-06-10.json';
 import dataBlog from './src/blog/posts/data.js';
 import fs from 'fs';
 
@@ -20,10 +16,6 @@ const pageData = {
   '/index.html': dataHome,
   '/blog/index.html': { articles: dataBlog },
   '/blog/plants_2020-06-06.html': data20200606,
-  '/blog/plants_2020-06-07.html': data20200607,
-  '/blog/plants_2020-06-08.html': data20200608,
-  '/blog/plants_2020-06-09.html': data20200609,
-  '/blog/plants_2020-06-10.html': data20200610,
   '/about/index.html': {
     title: 'About',
   },
@@ -39,10 +31,6 @@ export default defineConfig({
         main: resolve(__dirname, 'src/index.html'),
         blog: resolve(__dirname, 'src/blog/index.html'),
         post1: resolve(__dirname, 'src/blog/plants_2020-06-06.html'),
-        post2: resolve(__dirname, 'src/blog/plants_2020-06-07.html'),
-        post3: resolve(__dirname, 'src/blog/plants_2020-06-08.html'),
-        post4: resolve(__dirname, 'src/blog/plants_2020-06-09.html'),
-        post5: resolve(__dirname, 'src/blog/plants_2020-06-10.html'),
         about: resolve(__dirname, 'src/about/index.html'),
       },
     },
