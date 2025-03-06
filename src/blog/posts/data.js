510f609e09694524a9aa2a39ad5bcf2205c41bde
Max Bronner <88713269+max-bronner@users.noreply.github.com>
2025-03-06 21:39:35 +0100

    Add post to post list

--- src/blog/posts/data.js
+++ src/blog/posts/data.js
@@ -1,8 +1,9 @@
 import data20200606 from './plants_2020-06-06.json';
 import data20200615 from './plants_2020-06-15.json';
 import data20201109 from './plants_2020-09-09.json';
+import data20230615 from './dev_2023-06-15.json';
 
-const data = [data20200606, data20200615, data20201109];
+const data = [data20200606, data20200615, data20201109, data20230615];
 
 export default data.sort((a, b) => {
   return new Date(b.date) - new Date(a.date);
