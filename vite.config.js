93121e2760862fe8b1ff068b1daa20aadef8f3fd
Max Bronner <88713269+max-bronner@users.noreply.github.com>
2025-03-28 18:23:00 +0100

    Add dynamic content mapping of posts

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
+import fs from 'fs';
 import { ViteMinifyPlugin } from 'vite-plugin-minify';
 import Handlebars from 'vite-plugin-handlebars';
 import { HotReloadHbs, helpers } from './vite.plugin.js';
@@ -17,18 +18,30 @@ import dataBlog from './src/blog/posts/data.js';
 const pageData = {
   '/index.html': dataHome,
   '/blog/index.html': { articles: dataBlog },
-  '/blog/plants_2020-06-06.html': data20200606,
-  '/blog/plants_2020-06-15.html': data20200615,
-  '/blog/plants_2020-09-09.html': data20201109,
-  '/blog/dev_2021-10-28.html': data20211028,
-  '/blog/dev_2022-11-04.html': data20221104,
-  '/blog/dev_2023-06-15.html': data20230615,
-  '/blog/general_2025-02-18.html': data20250218,
-  '/blog/dev_2025-03-26.html': data20250326,
   '/about/index.html': {
     title: 'About',
   },
 };
+
+const rollupInput = {
+  main: resolve(__dirname, 'src/index.html'),
+  blog: resolve(__dirname, 'src/blog/index.html'),
+  about: resolve(__dirname, 'src/about/index.html'),
+};
+
+const postDir = './src/blog/posts';
+const posts = fs.readdirSync(postDir).filter((file) => file !== 'data.js');
+
+posts.forEach((post) => {
+  console.log(post);
+  const postName = post.replace('.json', '');
+  const postPath = `/blog/${post.replace('.json', '')}.html`;
+  const postData = JSON.parse(fs.readFileSync(`${postDir}/${post}`, 'utf-8'));
+
+  pageData[postPath] = postData;
+
+  rollupInput[postName] = resolve(__dirname, `./src/blog/post.html`);
+});
 
 export default defineConfig({
   root: 'src',
@@ -36,19 +49,7 @@ export default defineConfig({
     outDir: '../dist',
     emptyOutDir: true,
     rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'src/index.html'),
-        blog: resolve(__dirname, 'src/blog/index.html'),
-        post1: resolve(__dirname, 'src/blog/plants_2020-06-06.html'),
-        post2: resolve(__dirname, 'src/blog/plants_2020-06-15.html'),
-        post3: resolve(__dirname, 'src/blog/plants_2020-09-09.html'),
-        post4: resolve(__dirname, 'src/blog/dev_2021-10-28.html'),
-        post5: resolve(__dirname, 'src/blog/dev_2022-11-04.html'),
-        post6: resolve(__dirname, 'src/blog/dev_2023-06-15.html'),
-        post7: resolve(__dirname, 'src/blog/general_2025-02-18.html'),
-        post8: resolve(__dirname, 'src/blog/dev_2025-03-26.html'),
-        about: resolve(__dirname, 'src/about/index.html'),
-      },
+      input: rollupInput,
     },
   },
   css: {
