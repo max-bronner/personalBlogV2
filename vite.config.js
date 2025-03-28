17334fdb3435b4fd71dff63f2a33eee7345b63f5
Max Bronner <88713269+max-bronner@users.noreply.github.com>
2025-03-28 21:24:24 +0100

    Remove unused imports

--- vite.config.js
+++ vite.config.js
@@ -5,14 +5,6 @@ import { ViteMinifyPlugin } from 'vite-plugin-minify';
 import Handlebars from 'vite-plugin-handlebars';
 import { HotReloadHbs, helpers } from './vite.plugin.js';
 import dataHome from './src/data.js';
-import data20200606 from './src/blog/posts/plants_2020-06-06.json';
-import data20200615 from './src/blog/posts/plants_2020-06-15.json';
-import data20201109 from './src/blog/posts/plants_2020-09-09.json';
-import data20211028 from './src/blog/posts/dev_2021-10-28.json';
-import data20221104 from './src/blog/posts/dev_2022-11-04.json';
-import data20230615 from './src/blog/posts/dev_2023-06-15.json';
-import data20250218 from './src/blog/posts/general_2025-02-18.json';
-import data20250326 from './src/blog/posts/dev_2025-03-26.json';
 import dataBlog from './src/blog/posts/data.js';
 
 const pageData = {
