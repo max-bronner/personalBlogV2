89016a28890a14f709db3e46de60e6b7fe5649ed
Max Bronner <88713269+max-bronner@users.noreply.github.com>
2025-03-31 16:59:34 +0200

    Fix build setup

--- vite.config.js
+++ vite.config.js
@@ -31,6 +31,7 @@ posts.forEach((post) => {
   const postData = JSON.parse(fs.readFileSync(`${postDir}/${post}`, 'utf-8'));
 
   pageData[postPath] = postData;
+  rollupInput[postName] = resolve(__dirname, `src/blog/${postName}.html`);
 });
 
 export default defineConfig({
